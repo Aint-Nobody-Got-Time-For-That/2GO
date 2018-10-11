9051f2486fd09337beae1d6278ba423cb7cfa0e9
Victor Li <victorli5674@yahoo.com>
2018-10-10 19:32:21 -0700

    setup parse

---
+++ 2GO/KeysSample.swift
@@ -0,0 +1,19 @@
+//
+//  KeysSample.swift
+//  2GO
+//
+//  Created by Victor Li on 10/10/18.
+//  Copyright © 2018 Pat Khai. All rights reserved.
+//
+
+import Foundation
+
+// create a new file called Keys.swift and copy this file
+// make sure to change enum name to APIKeys with your real API keys
+struct KeysSample {
+    static let APP_ID = ""
+    static let MASTER_KEY = ""
+    static let MONGODB_URI = ""
+    static let PARSE_MOUNT = ""
+    static let SERVER_URL = ""
+}
