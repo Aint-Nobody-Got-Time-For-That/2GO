5d770f2ffc5db99eadb45944570e9e0b087893f3
Victor Li <victorli5674@yahoo.com>
2018-10-11 19:58:44 -0700

    fake data for restaurants and created folders for view controllers, views, and models

---
+++ 2GO/Model/MenuItem.swift
@@ -0,0 +1,26 @@
+//
+//  MenuItem.swift
+//  2GO
+//
+//  Created by Victor Li on 10/11/18.
+//  Copyright © 2018 Pat Khai. All rights reserved.
+//
+
+import Foundation
+
+struct MenuItem {
+    let id: Int
+    let photo: [String]
+    let description: String
+    
+    // TODO: might not need these properties right now.
+//    let rating: Double
+//    let reviews: [String]
+//    let allegeries: String
+    
+    init(id: Int, photo: [String], description: String) {
+        self.id = id
+        self.photo = photo
+        self.description = description
+    }
+}
---
+++ 2GO/Model/Restaurant.swift
@@ -0,0 +1,33 @@
+//
+//  Restaurant.swift
+//  2GO
+//
+//  Created by Victor Li on 10/11/18.
+//  Copyright © 2018 Pat Khai. All rights reserved.
+//
+
+import Foundation
+
+struct Restaurant {
+    let id: Int
+    let name: String
+    let photos: [String]
+    let categories: [String]
+    let address: String
+    let latitude: Double
+    let longitude: Double
+    let phoneNumber: String
+    let menuItems: [MenuItem]
+    
+    init(id: Int, name: String,photos: [String], categories: [String], address: String, latitude: Double, longitude: Double, phoneNumber: String, menuItems: [MenuItem]) {
+        self.id = id
+        self.name = name
+        self.photos = photos
+        self.categories = categories
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+        self.phoneNumber = phoneNumber
+        self.menuItems = menuItems
+    }
+}
